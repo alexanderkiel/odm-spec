7daad66f7ed6e71c35817371b68bf377e2cd514c
Alexander Kiel <alexanderkiel@gmx.net>
2018-06-07 13:30:33 +0200

    Release v0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/odm-spec "0.3-SNAPSHOT"
+(defproject org.clojars.akiel/odm-spec "0.2"
   :description "Clojure specs for ODM data structures."
   :url "https://github.com/alexanderkiel/odm-spec"
   :license {:name "Eclipse Public License"
